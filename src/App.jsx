95fda67659c1fa16e83d9b339f55e8219e543718
sachinthacham <sachinthachamindu26@gmail.com>
2025-04-27 13:14:49 +0530

    create dynamic button using usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,59 @@
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import reactImage from "./assets/react-core-concepts.png";
+import TabButton from "./components/TabButton";
+import { useState } from "react";
 
 function App() {
+  const [content, setContent] = useState("dynamic text goes here");
+  const handleClick = (topic) => {
+    setContent(topic);
+  };
   return (
     <div>
       <Header />
       <main>
-        <section id="core-concepts"></section>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcept
+              img={reactImage}
+              title="first title"
+              description="first conmponot decscription"
+            />
 
-        <CoreConcept
-          img={reactImage}
-          title="first title"
-          description="first conmponot decscription"
-        />
-        <CoreConcept
-          img={reactImage}
-          title="second title"
-          description="second conmponot decscription"
-        />
-        <CoreConcept
-          img={reactImage}
-          title="third title"
-          description="third conmponot decscription"
-        />
-        <CoreConcept
-          img={reactImage}
-          title="fourth title"
-          description="forth conmponot decscription"
-        />
+            <CoreConcept
+              img={reactImage}
+              title="second title"
+              description="second conmponot decscription"
+            />
+
+            <CoreConcept
+              img={reactImage}
+              title="third title"
+              description="third conmponot decscription"
+            />
+
+            <CoreConcept
+              img={reactImage}
+              title="fourth title"
+              description="forth conmponot decscription"
+            />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onClick={() => handleClick("components")}>
+              Components
+            </TabButton>
+            <TabButton onClick={() => handleClick("jsx")}>Jsx</TabButton>
+            <TabButton onClick={() => handleClick("props")}>Props</TabButton>
+            <TabButton onClick={() => handleClick("state")}>State</TabButton>
+          </menu>
+          <section>
+            <h2>{content}</h2>
+          </section>
+        </section>
       </main>
     </div>
   );
