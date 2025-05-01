9dca717528c09e4c1a81d06016285b92dbb82ca8
sachinthacham <sachinthachamindu26@gmail.com>
2025-05-01 17:08:56 +0530

    enhance the structure of the code

--- src/components/CoreConcept.jsx
+++ src/components/CoreConcept.jsx
@@ -1,7 +1,7 @@
 export default function CoreConcept(props) {
   return (
     <li>
-      <img src={props.img} />
+      <img src={props.image} />
       <h2>{props.title}</h2>
       <p>{props.description}</p>
     </li>
